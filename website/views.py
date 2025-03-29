3ff264418936f63c09432f3b9e5bc932f11d856d
Florea Alex <floreaalexmihai@yahoo.com>
2025-03-29 19:15:42 +0200

    Created record view,template,url

--- website/views.py
+++ website/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
+
 # Create your views here.
 
 def home(request):
+
+	records = Record.objects.all()
+
 
 	# Check to see if logging in
 	if request.method =="POST":
@@ -20,7 +25,7 @@ def home(request):
 			messages.success(request,"Username or password inccorect. Try again!")
 			return redirect('home')	
 	else:
-		return render(request,'home.html',{});	
+		return render(request,'home.html',{'records': records});	
 	
 
 
@@ -47,3 +52,13 @@ def register_user(request):
 		form = SignUpForm()	
 		return render(request,'register.html',{'form':form});
 	return render(request,'register.html',{'form':form});
+
+
+def user_record(request, pk):
+	if request.user.is_authenticated:
+		#Look up records
+		user_record = Record.objects.get(id = pk)
+		return render(request,'record.html',{'user_record': user_record});
+	else:
+		messages.success(request,"You must be logged in to view this page.")
+		return redirect('home')
