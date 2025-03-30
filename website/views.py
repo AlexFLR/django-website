e62e217ed9d78e2230c99f7855bc75efd924896f
Florea Alex <floreaalexmihai@yahoo.com>
2025-03-30 11:35:07 +0300

    Added delete function

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -73,3 +73,17 @@ def delete_record (request, pk):
 	else:
 		messages.success(request,'U must be logged in to delete this record!')
 		return redirect( 'home')
+
+
+def add_record(request):
+		form = AddRecordForm(request.POST or None)
+		if request.user.is_authenticated:
+			if request.method == "POST":
+				if form.is_valid():
+					add_record = form.save()
+					messages.success(request,"Record Added")
+					return redirect('home')	
+			return render(request,'add_record.html',{'form':form});
+		else:
+			messages.success("U are not authenticated. U must be logged in! ")
+			return redirect('home')		
